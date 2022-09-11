b6fcc9ba9862759cad330dfc3c149b2d2d248e5f
Mostafiz <mostafiz@iMac-Mostafizur.lan>
2022-09-11 11:05:17 +0100

    error fixed for  broken image

--- js/app.js
+++ js/app.js
@@ -13,14 +13,14 @@ loadProducts('https://fakestoreapi.com/products');
 
 // show all product in UI
 const showProducts = (products) => {
-   
+   console.log(products)
    setInnerText('total_products', products.length);
 
    document.getElementById("all-products").innerHTML = "";
 
    const allProducts = products.slice(0, 10).map((pd) => pd);
    for (const product of allProducts) {
-      const image = product.images;
+      const image = product.image;
       const div = document.createElement('div');
       div.classList.add('product');
       div.innerHTML = `<div class="single-product">
@@ -114,9 +114,9 @@ const updateTotal = () => {
 document.getElementById("search-btn").addEventListener("click", function () {
    const inputField = document.getElementById("input-value").value;
    const searchedProduct = arr[0].find((p) =>
-     p.category.startsWith(`${inputField}`)
+      p.category.startsWith(`${inputField}`)
    );
    showProducts(searchedProduct);
- });
+});
 
 
